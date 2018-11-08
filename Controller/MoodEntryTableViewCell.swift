ba96b0aee568bb09df2393c4e6fec393c913816a
Rinni Swift <rinradaswift@gmail.com>
2018-11-08 15:38:37 -0800

    tableviewcell showing correct color with the correct mood

---
+++ Controller/MoodEntryTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MoodEntryTableViewCell.swift
+//  moodTracker
+//
+//  Created by Rinni Swift on 11/8/18.
+//  Copyright © 2018 Rinni Swift. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MoodEntryTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var labelMoodTitle: UILabel!
+    @IBOutlet weak var labelMoodDate: UILabel!
+    @IBOutlet weak var imageViewMoodColor: UIImageView!
+    
+}
--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -11,12 +11,43 @@ import UIKit
 class ViewController: UIViewController {
     
     var entries = [MoodEntry]()
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        
+        let goodEntry = MoodEntry(mood: .good, date: Date())
+        let neutralEntry = MoodEntry(mood: .neutral, date: Date())
+        let badEntry = MoodEntry(mood: .bad, date: Date())
+        
+        entries = [goodEntry, neutralEntry, badEntry]
+        tableView.reloadData()
     }
 
 
 }
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return entries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "moodEntryCell", for: indexPath) as! MoodEntryTableViewCell
+        
+        let entry = entries[indexPath.row]
+        cell.labelMoodTitle.text = entry.mood.stringValue
+        cell.labelMoodDate.text = String(describing: entry.date)
+        cell.imageViewMoodColor.backgroundColor = entry.mood.colorValue
+        
+        return cell
+    }
+    
+    
+}
+
+
 
